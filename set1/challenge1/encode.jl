e3a7d44f81767b52504b9f5f7ab9d2603f1e2d0a
Billy Brickner <billybrickner@gmail.com>
2021-07-04 23:36:17 -0400

    Streamlined s1ch1 code a bit

--- set1/challenge1/encode.jl
+++ set1/challenge1/encode.jl
@@ -1,9 +1,9 @@
+#! /usr/bin/env julia
+
 open("cypher.txt") do f
-    while ! eof(f)
-        s = readline(f)
-        for i in range(1,step=2,stop=length(s))
-            convert = s[i:i+1]
-            print(Char(parse(Int, convert, base=16)))
-        end
+    s = readline(f)
+    for i in range(1,step=2,stop=length(s))
+        convert = s[i:i+1]
+        print(Char(parse(Int64, convert, base=16)))
     end
 end
